9324f36eba231c89500a11e3e7026b7aa03c5bed
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-06-01 12:43:27 -0800

    add week 5  updated version of ToDoList
    co-authored-by: Min Chang <minchangmhc@gmail.com>

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using ToDoList.Models;
-
+using Microsoft.AspNetCore.Identity;
 namespace ToDoList
 {
   public class Startup
@@ -25,11 +25,27 @@ namespace ToDoList
       services.AddEntityFrameworkMySql()
       .AddDbContext<ToDoListContext>(options => options
       .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<ToDoListContext>()
+        .AddDefaultTokenProviders();
+
+      services.Configure<IdentityOptions>(options =>
+      {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredUniqueChars = 0;
+      });
     }
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
+      app.UseAuthentication();
       app.UseRouting();
+      app.UseAuthorization();
 
       app.UseEndpoints(routes =>
       {
